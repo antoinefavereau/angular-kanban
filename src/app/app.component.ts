71e2aa35e0e434f6438bbcf2b2ec386a5c0a5d8b
Antoine Favereau <antoinefavereau45@gmail.com>
2024-08-19 14:34:46 +0200

    fix mat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Task } from './task/task';
 import { TaskComponent } from './task/task.component';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TaskComponent, MatButtonModule],
+  imports: [CommonModule, RouterOutlet, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
